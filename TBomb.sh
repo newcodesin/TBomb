2466b4bf1068cb32d21c98791c13c5fb8f028000
newcodesin <94756343+newcodesin@users.noreply.github.com>
2023-04-09 11:09:32 +0530

    Update TBomb.sh

--- TBomb.sh
+++ TBomb.sh
@@ -35,7 +35,7 @@ banner() {
         echo -e "\e[4;34m This Bomber Was Created By \e[1;NITHISH \e[0m"
     else
         echo -e "\e[1;34mCreated By \e[1;34m"
-        toilet -f mono12 -F border SpeedX
+        toilet -f mono12 -F border BNR
     fi
     echo -e "\e[1;34m For Any Queries Join Me!!!\e[0m"
     echo -e "\e[1;32m           Insta @beereddynithish \e[0m"
